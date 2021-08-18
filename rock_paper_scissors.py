d45a9cf0e7016a82f150a480ebdd81a7f395fc99
Jordan <jordankennedy2018@outlook.com>
2021-08-18 09:07:24 -0400

    Added scorekeeping system

--- rock_paper_scissors.py
+++ rock_paper_scissors.py
@@ -10,12 +10,16 @@ def play(userInput, computerInput):
         print("It's a tie!")
     elif userInput == "rock" and computerInput == "paper":
         print("Paper covers rock.  You lose!")
+        return "computer"
     elif userInput == "paper" and computerInput == "scissors":
         print("Scissors cut paper.  You lose!")
+        return "computer"
     elif userInput == "scissors" and computerInput == "rock":
         print("Rock crushes scissors.  You lose!")
+        return "computer"
     else:
         print("You win!  Congratulations!")
+        return "human"
 
 def computerPick():
     randomNum = random.randint(0, 2)
@@ -30,18 +34,24 @@ def computerPick():
 
 playerScore = 0
 computerScore = 0
+
+
 playing = True
 
 print("Hello!  What is your name?", end = "")
 playerName = input()
 print()
 
-print(f"Hi {playerName}.  Do you want to play rock, paper, scissors? (yes/no)", end="")
+print(f'Hi {playerName}.  Do you want to play rock, paper, scissors? (yes/no)', end="")
 wantToPlay = input().lower()
 if wantToPlay == "no":
+    print("Goodbye")
     playing = False
+print()
 
 while playing == True:
+    winner = "computer"
+    
     print("It's time to play.  Pick rock, paper, or scissors.", end="")
     userChoice = input().lower()
     print()
@@ -49,13 +59,17 @@ while playing == True:
     computerChoice = computerPick()
     print(f"The computer chose {computerChoice}.", end= " ")
     
-    play(userChoice, computerChoice)
+    winner = play(userChoice, computerChoice)
     print()
-    
-    
-    print("The current score is {playerName}: {playerScore} and Computer: {computerScore}.")
+    if winner == "computer":
+        computerScore += 1
+    elif winner == "human":
+        playerScore += 1
+      
+    print(f"The current score is {playerName}: {playerScore} and Computer: {computerScore}.")
     print("Play again? (yes/no)", end ="")
     playAgain = input().lower()
+    print()
     if playAgain == "no":
         print("Goodbye")
         playing = False
